389c9d87d35fabd2e773223afb70666b4400d38e
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-25 02:26:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
   # code here
   counter = 0
   while counter <= words.length - 1
-    words[counter]
+    yield(words[counter])
     counter += 1
   end
 end
