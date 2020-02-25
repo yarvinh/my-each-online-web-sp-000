53c4ed062f45cd1548c1fb8570d9f2ecd70ff5a4
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-25 02:33:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
   # code here
+  counter = 0
+  while counter <= words.length - 1
+    yield(words[counter])
+    counter += 1
+  end
+  words
 end
